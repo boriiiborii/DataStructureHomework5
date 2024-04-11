4bc1598d90e39d296aa2396fe3eae2aeb8328e0f
Haena <ghkwlsspdl@naver.com>
2024-04-11 17:39:01 +0900

    함수 선언 및 행렬 크기 받아오기

---
+++ matrix.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void printMatrix();
+void additionMatrix();
+void subtractionMatrix();
+void transposeMatrix();
+void multiplyMatrix();
+void freeMatrix();
+
+
+void main() {
+    printf("[----- [leeHwaJin]  [2020037103] -----]\n");
+    int rows, cols;
+
+    printf("A,B의 행과 열을 입력하라: \n");
+    scanf("%d %d", &rows, &cols);
+}
+
+void printMatrix() {
+   
+}
+
+void additionMatrix() {
+   
+}
+
+
+void subtractionMatrix() {
+   
+}
+
+
+void transposeMatrix() {
+   
+}
+
+
+void multiplyMatrix() {
+
+}
+
+
+void freeMatrix() {
+
+}
