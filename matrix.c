31196afe5e1bbad3f7058ea5b16c9c30a0c04259
Haena <ghkwlsspdl@naver.com>
2024-04-11 17:42:25 +0900

    동적 메모리 할당

--- matrix.c
+++ matrix.c
@@ -14,7 +14,14 @@ void main() {
 
     printf("A,B의 행과 열을 입력하라: \n");
     scanf("%d %d", &rows, &cols);
-}
+
+    //동적 메모리 할당
+    int **matrixA = (int **)malloc(rows * sizeof(int *));
+    int **matrixB = (int **)malloc(rows * sizeof(int *));
+    for (int i = 0; i < rows; i++) {
+        matrixA[i] = (int *)malloc(cols * sizeof(int));
+        matrixB[i] = (int *)malloc(cols * sizeof(int));
+    }
 
 void printMatrix() {
    
